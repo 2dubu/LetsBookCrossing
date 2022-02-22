cfb3fc51758a51c772171a7b683295393228e376
dlrjsdn5012 <dlrjsdn5012@naver.com>
2022-02-22 11:25:40 +0900

    add CheckVC

---
+++ BookBori/BookBori/ViewController/Check/CheckViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CheckViewController.swift
+//  BookBori
+//
+//  Created by 이건우 on 2022/02/22.
+//
+
+import UIKit
+
+class CheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
