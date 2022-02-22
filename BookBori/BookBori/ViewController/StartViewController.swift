97d55477e8fdf3ed8b0f7292de9d969b4f08d645
dlrjsdn5012 <dlrjsdn5012@naver.com>
2022-02-22 12:05:05 +0900

    LaunchScreen, StartVC 임시 구현

--- BookBori/BookBori/ViewController/StartViewController.swift
+++ BookBori/BookBori/ViewController/StartViewController.swift
@@ -22,7 +22,6 @@ class StartViewController: UIViewController {
             self.init(getWasLaunchedBefore: { userDefaults.bool(forKey: key) }, setWasLaunchedBefore: { userDefaults.set($0, forKey: key) })
         }
     }
-
     
     // 첫 실행인지 확인
     let fistLaunch = FirstLaunch(userDefaults: .standard, key: "com.any-suggestion.FirstLaunch.WasLaunchedBefore")
@@ -37,31 +36,27 @@ class StartViewController: UIViewController {
     
     //MARK: - function
     func checkDeviceNetworkStatus() {
-            if(DeviceManager.shared.networkStatus) {
-                // 네트워크 연결 O
-                getBannerData {
-                    getShelfData {
-                        let mainNav = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainNC")
-                        mainNav.modalPresentationStyle = .fullScreen
-                        mainNav.modalTransitionStyle = .crossDissolve
-                        self.present(mainNav, animated: true, completion: nil)
-                        UIView.transition(with: self.view, duration: 1.5, options: .transitionCrossDissolve, animations: nil, completion: nil)
-                    }
-                }
-            } else {
-                // 네트워크 연결 X
-                let alert : UIAlertController = UIAlertController(title: "서버에 연결할 수 없습니다",
-                    message: """
+        if(DeviceManager.shared.networkStatus) {
+            // 네트워크 연결 O
+            let mainNav = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainNC")
+            mainNav.modalPresentationStyle = .fullScreen
+            mainNav.modalTransitionStyle = .crossDissolve
+            self.present(mainNav, animated: true, completion: nil)
+            UIView.transition(with: self.view, duration: 1.5, options: .transitionCrossDissolve, animations: nil, completion: nil)
+        } else {
+            // 네트워크 연결 X
+            let alert : UIAlertController = UIAlertController(title: "서버에 연결할 수 없습니다",
+                                                              message: """
                     네트워크가 연결되지 않았습니다.
                     Wi-Fi 또는 데이터를 활성화 해주세요.
                     """,
-                    preferredStyle: .alert)
-                let action: UIAlertAction = UIAlertAction(title: "다시 시도", style: .default, handler: { (ACTION) in
-                    self.checkDeviceNetworkStatus()
-                })
-                action.setValue(UIColor(#colorLiteral(red: 0.6823529412, green: 0.5725490196, blue: 0.4039215686, alpha: 1)), forKey: "titleTextColor")
-                alert.addAction(action)
-                present(alert, animated: true, completion: nil)
-            }
+                                                              preferredStyle: .alert)
+            let action: UIAlertAction = UIAlertAction(title: "다시 시도", style: .default, handler: { (ACTION) in
+                self.checkDeviceNetworkStatus()
+            })
+            action.setValue(UIColor(#colorLiteral(red: 0.6823529412, green: 0.5725490196, blue: 0.4039215686, alpha: 1)), forKey: "titleTextColor")
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
     }
 }
