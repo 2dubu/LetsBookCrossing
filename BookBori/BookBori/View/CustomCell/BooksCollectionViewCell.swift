dd33124964135bfe0de42e2d9f3af3bc8f6aac2b
lru0409 <74223246+lru0409@users.noreply.github.com>
2022-02-22 11:33:35 +0900

    BooksCollectionViewCell 파일 생성

---
+++ BookBori/BookBori/View/CustomCell/BooksCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BooksCollectionViewCell.swift
+//  BookBori
+//
+//  Created by 이로운 on 2022/02/22.
+//
+
+import UIKit
+
+class BooksCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
