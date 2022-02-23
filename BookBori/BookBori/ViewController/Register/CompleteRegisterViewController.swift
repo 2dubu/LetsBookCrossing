9dcbf9b85482f0e4d3db7b51d3163a410aa88a7d
dlrjsdn5012 <dlrjsdn5012@naver.com>
2022-02-23 20:58:46 +0900

    popToViewController 버그 수정

--- BookBori/BookBori/ViewController/Register/CompleteRegisterViewController.swift
+++ BookBori/BookBori/ViewController/Register/CompleteRegisterViewController.swift
@@ -47,37 +47,46 @@ class CompleteRegisterViewController: UIViewController {
     //MARK: - IBAction
     @IBAction func confirmButtonTapped(_ sender: Any) {
         
-        let mainSB = UIStoryboard(name: "Main", bundle: nil)
-        let mainNC = mainSB.instantiateViewController(withIdentifier: "MainNC")
-        
         // 이때 에니메이션 스타일을 모달 내리는 것 처럼 하는 건 어떨까 고려
-        self.navigationController?.popToViewController(mainNC, animated: true)
+        let controllers = self.navigationController?.viewControllers
+        for vc in controllers! {
+            if vc is MainViewController {
+                _ = self.navigationController?.popToViewController(vc as! MainViewController, animated: true)
+            }
+        }
     }
     
     //MARK: - Functions
     func setElements() {
         
-        let fontsize = 12
+        let fontsize : CGFloat = 10
         
-        self.navigationItem.title = "도서 교환 정보"
-        self.exchangedBookLabel.text = "교환 도서"
-        self.seoulBookBogoLabel.text = "서울책보고"
+        navigationItem.title = "도서 교환 정보"
         
-        self.checkImageView.image = UIImage(systemName: "checkmark")
-        self.descriptionTitle.text = "신청이 완료되었습니다."
+        exchangedBookLabel.text = "교환 도서"
+        seoulBookBogoLabel.text = "서울책보고"
         
-        self.descriptionLabel1.text = """
+        exchangedBookImageView.image = UIImage(named: "1")
+        seoulBookBogoImageView.image = UIImage(named: "2")
+        
+        exchangedBookTitle.text = "고양이는 츄르를 좋아해"
+        seoulBookBogoBookTitle.text = "강아지는 개껌을 좋아해"
+        
+        checkImageView.image = UIImage(systemName: "checkmark")
+        descriptionTitle.text = "신청이 완료되었습니다."
+        
+        descriptionLabel1.text = """
         \(calculateDate()) 오후 7시까지 "서울책보고"에 방문하신 후 교환을 완료하여 주시기 바랍니다.
         시간 내에 교환이 완료되지 않을 경우 자동으로 취소 처리됩니다.
         """
         let attributedStr = NSMutableAttributedString(string: self.descriptionLabel1.text ?? "")
         attributedStr.addAttribute(.foregroundColor, value: UIColor.red, range: (self.descriptionLabel1.text! as NSString).range(of: "시간 내에 교환이 완료되지 않을 경우 자동으로 취소 처리됩니다."))
         attributedStr.addAttribute(.font, value: UIFont.systemFont(ofSize: 17, weight: .bold), range: (self.descriptionLabel1.text! as NSString).range(of: "시간 내에 교환이 완료되지 않을 경우 자동으로 취소 처리됩니다."))
-        self.descriptionLabel1.attributedText = attributedStr
+        descriptionLabel1.attributedText = attributedStr
         
-        self.descriptionLabel2.text = "* 교환 취소를 원하실 경우 \"북크로싱 신청 내역 조회\"에서 취소할 수 있습니다."
+        descriptionLabel2.text = "* 교환 취소를 원하실 경우 \"북크로싱 신청 내역 조회\"에서 취소할 수 있습니다."
         
-        self.confirmButton.setTitle("확인", for: .normal)
+        confirmButton.setTitle("확인", for: .normal)
         
     }
     
