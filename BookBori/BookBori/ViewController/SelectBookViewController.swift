3eac7f5aac095e9fac4c038417c63d9579b48fe0
lru0409 <74223246+lru0409@users.noreply.github.com>
2022-02-23 11:19:10 +0900

    SelectBookVC 검색 기능 구현 완료

--- BookBori/BookBori/ViewController/SelectBookViewController.swift
+++ BookBori/BookBori/ViewController/SelectBookViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class SelectBookViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
+    //MARK: - constant, variable
+    
     let searchBar = UISearchBar()
-    var titleArray : [String]?
-    var filterArray : [String]?
-
-    @IBOutlet weak var booksCollectionView: UICollectionView!
+    var filteredArray : [Book] = BookDummyData.shared.books  // 검색에 맞는 데이터만 필터링해서 담는 Book 배열
+    
+    
+    //MARK: - viewController Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,33 +27,38 @@ class SelectBookViewController: UIViewController, UICollectionViewDelegate, UICo
         initSearchBar()
     }
     
+
+    //MARK: - IBAction, IBOutlet
+    
     @IBAction func backButtonTapped(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+    @IBOutlet weak var booksCollectionView: UICollectionView!
+    
+    
+    //MARK: - function
     
     private func initSearchBar() {
         searchBar.text = ""
         searchBar.placeholder = "책을 검색해 보세요"
         self.navigationItem.titleView = searchBar
-        searchBar.setImage(UIImage(), for: UISearchBar.Icon.search, state: .normal)
+        //searchBar.setImage(UIImage(), for: UISearchBar.Icon.search, state: .normal)
         searchBar.searchTextField.backgroundColor = UIColor.clear
-        searchBar.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     }
     
     
     //MARK: - collectionView
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return BookDummyData.shared.books.count
+        return filteredArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = booksCollectionView.dequeueReusableCell(withReuseIdentifier: "BookCell", for: indexPath) as! BooksCollectionViewCell
         
-        let book = BookDummyData.shared.books[indexPath.row]
-        cell.titleLabel.text = book.title
-        cell.coverImageView.image = UIImage(named: book.image)
+        cell.titleLabel.text = filteredArray[indexPath.row].title
+        cell.coverImageView.image = UIImage(named: filteredArray[indexPath.row].image)
         
         return cell
     }
@@ -66,16 +73,13 @@ class SelectBookViewController: UIViewController, UICollectionViewDelegate, UICo
         return 0
     }
 
-    // cell 사이즈( 옆 라인을 고려하여 설정 )
+    // cell 사이즈
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
         let size = CGSize(width: collectionView.frame.width/3, height: collectionView.frame.width/3+10)
-        
         return size
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
         let guideVC = UIStoryboard(name: "Apply", bundle: nil).instantiateViewController(withIdentifier: "GuideVC")
         self.navigationController?.pushViewController(guideVC, animated: true)
     }
@@ -83,35 +87,29 @@ class SelectBookViewController: UIViewController, UICollectionViewDelegate, UICo
     
     //MARK: - searchBar
     
+    // 검색 바에 텍스트가 비어있을 때 alert 표시
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
-        print("검색!")
-        
         guard let text = searchBar.text else { return }
-        
         if text.trimmingCharacters(in: .whitespaces).isEmpty {
-        
             self.present(UtilitiesForAlert.returnAlert(title: "검색어를 입력해 주세요", msg: "", buttonTitle: "확인", handler: nil), animated: true, completion: nil)
-            
-        } else {
-            
-            
-            
-            
         }
-        
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 검색 바 텍스트가 바뀔 때마다 검색 결과 업데이트
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let text = searchBar.text else { return }
+        filteredArray.removeAll()
+        for i in BookDummyData.shared.books {
+            if i.title.lowercased().contains(text.lowercased()) {
+                filteredArray.append(i)
+            }
+        }
+        // 검색 바에 텍스트가 비어있을 때는 모든 데이터 표시
+        if text.trimmingCharacters(in: .whitespaces).isEmpty == true {
+            filteredArray = BookDummyData.shared.books
+        }
+        self.booksCollectionView.reloadData()
     }
-    */
+    
 
 }
