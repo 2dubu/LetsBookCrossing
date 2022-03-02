85d0778e42e8c15df39d67ff1748680a7865b149
lru0409 <74223246+lru0409@users.noreply.github.com>
2022-03-02 10:38:27 +0900

    CollectUserInfoView 전번 textField 하이픈(-) 제대로 추가 및 삭제

--- BookBori/BookBori/ViewController/Apply/CollectUserInfoViewController.swift
+++ BookBori/BookBori/ViewController/Apply/CollectUserInfoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CollectUserInfoViewController: UIViewController {
+class CollectUserInfoViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: - viewController Life Cycle
 
@@ -21,9 +21,8 @@ class CollectUserInfoViewController: UIViewController {
         phoneNumberTextField.keyboardType = .numberPad
         passwordTextField.keyboardType = .numberPad
         
-        NotificationCenter.default.addObserver(self, selector: #selector(phoneNumberDidChange(_:)), name: UITextField.textDidChangeNotification, object: phoneNumberTextField)
-        NotificationCenter.default.addObserver(self, selector: #selector(passwordDidChange(_:)), name: UITextField.textDidChangeNotification, object: passwordTextField)
-
+        phoneNumberTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
     
@@ -51,7 +50,6 @@ class CollectUserInfoViewController: UIViewController {
             guard let phoneNumberText = phoneNumberTextField.text else { return }
             if ((phoneNumberRegex?.firstMatch(in: phoneNumberText, options: [], range: NSRange(location: 0, length: phoneNumberText.count))) != nil) {
                 phoneNumberSuitable = true
-                print("전번 굿")
             }
             
             // 비밀번호 검사
@@ -59,7 +57,6 @@ class CollectUserInfoViewController: UIViewController {
             guard let passwordText = passwordTextField.text else { return }
             if ((passwordRegex?.firstMatch(in: passwordText, options: [], range: NSRange(location: 0, length: passwordText.count))) != nil) {
                 passwordSuitable = true
-                print("비번 굿")
             }
             
             if phoneNumberSuitable == true && passwordSuitable == true {
@@ -76,14 +73,48 @@ class CollectUserInfoViewController: UIViewController {
         }
     }
     
+    var pastCount : Int?
     @IBAction func phoneNumberTextFieldEditingChanged(_ sender: Any) {
         updateContinueButton()
+        
+        guard let text = phoneNumberTextField.text else { return }
+        
+        if phoneNumberTextField.text?.count ?? 0 > pastCount ?? 0 {
+            let maxLength = 13
+            if text.count == maxLength {
+                phoneNumberTextField.resignFirstResponder()
+            }
+            if text.count == 3 {
+                phoneNumberTextField.text! += "-"
+            } else if text.count == 8 {
+                phoneNumberTextField.text! += "-"
+            } else if text.count > maxLength {
+                let index = text.index(text.startIndex, offsetBy: maxLength)
+                let newString = text[text.startIndex..<index]
+                phoneNumberTextField.text = String(newString)
+            }
+        } else {
+            if phoneNumberTextField.text?.hasSuffix("-") == true {
+                phoneNumberTextField.text?.removeLast(1)
+            }
+        }
+        pastCount = phoneNumberTextField.text?.count
     }
     
     @IBAction func passwordTextFieldEditingChanged(_ sender: Any) {
         updateContinueButton()
+        if let text = passwordTextField.text {
+            let maxLength = 4
+            if text.count == maxLength {
+                passwordTextField.resignFirstResponder()
+            }
+            if text.count > maxLength {
+                let index = text.index(text.startIndex, offsetBy: maxLength)
+                let newString = text[text.startIndex..<index]
+                passwordTextField.text = String(newString)
+            }
+        }
     }
-    
     
     // MARK: - function
     
@@ -116,48 +147,49 @@ class CollectUserInfoViewController: UIViewController {
         self.view.endEditing(true)
     }
     
+    /*
     @objc private func phoneNumberDidChange(_ notification: Notification) {
-            if let textField = notification.object as? UITextField {
-                if let text = textField.text {
-                    
-                    let maxLength = 13
-                    
-                    if text.count == maxLength {
-                        textField.resignFirstResponder()
-                    }
-                    
-                    if text.count == 3 {
-                        textField.text! += "-"
-                    } else if text.count == 8 {
-                        textField.text! += "-"
-                    } else if text.count > maxLength {
-                        let index = text.index(text.startIndex, offsetBy: maxLength)
-                        let newString = text[text.startIndex..<index]
-                        textField.text = String(newString)
-                    }
+        if let textField = notification.object as? UITextField {
+            if let text = textField.text {
+                
+                let maxLength = 13
+                
+                if text.count == maxLength {
+                    textField.resignFirstResponder()
+                }
+                
+                if text.count == 3 {
+                    textField.text! += "-"
+                } else if text.count == 8 {
+                    textField.text! += "-"
+                } else if text.count > maxLength {
+                    let index = text.index(text.startIndex, offsetBy: maxLength)
+                    let newString = text[text.startIndex..<index]
+                    textField.text = String(newString)
                 }
             }
         }
+    }
     
     @objc private func passwordDidChange(_ notification: Notification) {
-            if let textField = notification.object as? UITextField {
-                if let text = textField.text {
-                    
-                    let maxLength = 4
-                    
-                    if text.count == maxLength {
-                        textField.resignFirstResponder()
-                    }
-                    
-                    if text.count > maxLength {
-                        let index = text.index(text.startIndex, offsetBy: maxLength)
-                        let newString = text[text.startIndex..<index]
-                        textField.text = String(newString)
-                    }
+        if let textField = notification.object as? UITextField {
+            if let text = textField.text {
+                
+                let maxLength = 4
+                
+                if text.count == maxLength {
+                    textField.resignFirstResponder()
+                }
+                
+                if text.count > maxLength {
+                    let index = text.index(text.startIndex, offsetBy: maxLength)
+                    let newString = text[text.startIndex..<index]
+                    textField.text = String(newString)
                 }
             }
         }
-    
+    }
+    */
 
     /*
     // MARK: - Navigation
