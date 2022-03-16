981c67b843e542e776ebbba23dce8df0e4af3c15
dlrjsdn5012 <dlrjsdn5012@naver.com>
2022-03-16 12:06:11 +0900

    DummyData 추가
    스크롤 테스트

--- BookBori/BookBori/Model/DummyData.swift
+++ BookBori/BookBori/Model/DummyData.swift
@@ -27,6 +27,18 @@ class BookDummyData {
         Book(title: "일 잘하는 사람은 단순하게 합니다", image: "1", author: "주주" , publisher: "귀여운 출판사" , yearPublished: 2011),
         Book(title: "생각 정리 습관", image: "2", author: "모모" , publisher: "아름다운 출판사" , yearPublished: 2013),
         Book(title: "생각 정리 습관", image: "2", author: "뿌뿌" , publisher: "완벽한 출판사" , yearPublished: 2018),
+        Book(title: "생각 정리 습관", image: "2", author: "디디" , publisher: "최고의 출판사" , yearPublished: 2020),
+        Book(title: "우주는 얼마나 넓을까?", image: "1", author: "미미" , publisher: "행복한 출판사" , yearPublished: 2000),
+        Book(title: "글쓰기 잘 하는 아이는 이렇게 시작합니다", image: "2", author: "나나" , publisher: "멋진 출판사" , yearPublished: 2010),
+        Book(title: "일 잘하는 사람은 단순하게 합니다", image: "1", author: "주주" , publisher: "귀여운 출판사" , yearPublished: 2011),
+        Book(title: "생각 정리 습관", image: "2", author: "모모" , publisher: "아름다운 출판사" , yearPublished: 2013),
+        Book(title: "생각 정리 습관", image: "2", author: "뿌뿌" , publisher: "완벽한 출판사" , yearPublished: 2018),
+        Book(title: "생각 정리 습관", image: "2", author: "디디" , publisher: "최고의 출판사" , yearPublished: 2020),
+        Book(title: "우주는 얼마나 넓을까?", image: "1", author: "미미" , publisher: "행복한 출판사" , yearPublished: 2000),
+        Book(title: "글쓰기 잘 하는 아이는 이렇게 시작합니다", image: "2", author: "나나" , publisher: "멋진 출판사" , yearPublished: 2010),
+        Book(title: "일 잘하는 사람은 단순하게 합니다", image: "1", author: "주주" , publisher: "귀여운 출판사" , yearPublished: 2011),
+        Book(title: "생각 정리 습관", image: "2", author: "모모" , publisher: "아름다운 출판사" , yearPublished: 2013),
+        Book(title: "생각 정리 습관", image: "2", author: "뿌뿌" , publisher: "완벽한 출판사" , yearPublished: 2018),
         Book(title: "생각 정리 습관", image: "2", author: "디디" , publisher: "최고의 출판사" , yearPublished: 2020)
     ]
 }
