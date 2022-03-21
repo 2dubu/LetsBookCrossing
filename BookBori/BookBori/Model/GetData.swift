e554e8fa662f9d7005fa532b88390748f198797a
dlrjsdn5012 <dlrjsdn5012@naver.com>
2022-03-21 10:28:13 +0900

    json 파싱 구현 완료 (소소야 서버)

--- BookBori/BookBori/Model/GetData.swift
+++ BookBori/BookBori/Model/GetData.swift
@@ -17,39 +17,37 @@ class SeoulBookBogoDataManager {
 }
 
 struct ApplicableBookList: Codable {
-    struct BookInfo: Codable{
-        let bookNo: String?
-        let imgUrl: String
-        let bookTitle: String?
-        let author: String?
-        let publisher: String?
-        let publicationDate: String?
-        let isbnNo: String?
-    }
+    var bookList: [BookInfo]
 }
 
-let urlString = "https://gschool.fortune8282.co.kr/booklist_json.txt"
+struct BookInfo: Codable {
+    let bookNo: String
+    let imgUrl: String
+    let bookTitle: String
+    let author: String
+    let publisher: String
+    let publicationDate: String
+    let isbnNo: String
+}
+    
+let urlString = "https://gschool.fortune8282.co.kr/booklist_json.asp"
 
 func getBookList() {
     // HTTP Request
-    let headers: HTTPHeaders = ["Content-Type":"application/json"]
-    AF.request(urlString, headers: headers).responseJSON { (response) in
+    AF.request(urlString).responseJSON { (response) in
         switch response.result {
         case .success(let res):
             do {
                 let jsonData = try JSONSerialization.data(withJSONObject: res, options: .prettyPrinted)
                 let json = try JSONDecoder().decode(ApplicableBookList.self, from: jsonData)
 
-                // dataSource에 변환한 값을 대입
                 SeoulBookBogoDataManager.shared.applicableBookList = json
-                print("json: \(json)")
-                print("data: \(SeoulBookBogoDataManager.shared.applicableBookList)")
             } catch(let err) {
                 print(err.localizedDescription)
             }
             // 실패
         case .failure(let err):
-            print(err.localizedDescription)
+            print(err.localizedDescription, "실패")
         }
     }
 }
