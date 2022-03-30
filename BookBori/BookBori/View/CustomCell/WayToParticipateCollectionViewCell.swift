66dc953247ca48897a99337174919743477b4267
lru0409 <74223246+lru0409@users.noreply.github.com>
2022-03-30 09:38:02 +0900

    WayToParticipateCollectionViewCell 파일 생성

---
+++ BookBori/BookBori/View/CustomCell/WayToParticipateCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WayToParticipateCollectionViewCell.swift
+//  Pods
+//
+//  Created by 이로운 on 2022/03/30.
+//
+
+import UIKit
+
+class WayToParticipateCollectionViewCell: UICollectionViewCell {
+    
+}
