8f3acfacf7e1d5d15a7ee1cf420aa58cf9a42f7e
lru0409 <74223246+lru0409@users.noreply.github.com>
2022-04-11 12:31:13 +0900

    IsApplicableBook, IsApplicableUser, InfoApplyBook 데이터 모델 생성

--- BookBori/BookBori/Model/GetData.swift
+++ BookBori/BookBori/Model/GetData.swift
@@ -8,17 +8,16 @@
 import Foundation
 import Alamofire
 
-class SeoulBookBogoDataManager {
-    static let shared: SeoulBookBogoDataManager = SeoulBookBogoDataManager()
+class DataManager {
+    static let shared: DataManager = DataManager()
+    
     var applicableBookList: ApplicableBookList?
+    var isApplicableBook: IsApplicableBook?
+    var infoApplyBook: InfoApplyBook?
+    var isApplicableUser: IsApplicableUser?
     
     private init () {
     }
-}
-
-struct ApplicableBookList: Codable {
-    let header: Header?
-    let data: Data?
 }
 
 struct Header: Codable {
@@ -26,10 +25,17 @@ struct Header: Codable {
     let resultMessage: String
 }
 
-struct Data: Codable {
+//MARK: - SelectBookVC
+
+struct ApplicableBookList: Codable {
+    let header: Header?
+    let data: BookListData?
+}
+
+struct BookListData: Codable {
     let listCount: Int
     let nextPage: String
-    var bookList: [BookData]
+    let bookList: [BookData]
 }
 
 struct BookData: Codable {
@@ -49,8 +55,8 @@ func getApplicableBookList(pagesize: Int, page: Int, keyword: String, completion
                 let jsonData = try JSONSerialization.data(withJSONObject: res, options: .prettyPrinted)
                 let json = try JSONDecoder().decode(ApplicableBookList.self, from: jsonData)
 
-                SeoulBookBogoDataManager.shared.applicableBookList = json
-                print("succes!", SeoulBookBogoDataManager.shared.applicableBookList)
+                DataManager.shared.applicableBookList = json
+                print("succes!", DataManager.shared.applicableBookList)
             } catch(let err) {
                 print("err.localizedDescription", err.localizedDescription)
             }
@@ -61,3 +67,112 @@ func getApplicableBookList(pagesize: Int, page: Int, keyword: String, completion
         completion()
     }
 }
+
+//MARK: - IsApplicableBook
+
+struct IsApplicableBook: Codable {
+    let header: Header?
+    let data: isApplicableBookData?
+}
+
+struct isApplicableBookData: Codable {
+    let canApply: Bool
+}
+
+func getIsApplicableBook(bookPk: Int, completion: @escaping ()->()) {
+    let urlString = ""
+    
+    // HTTP Request
+    AF.request(urlString).responseJSON { (response) in
+        switch response.result {
+        case .success(let res):
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: res, options: .prettyPrinted)
+                let json = try JSONDecoder().decode(IsApplicableBook.self, from: jsonData)
+
+                DataManager.shared.isApplicableBook = json
+                print("succes!", DataManager.shared.isApplicableBook)
+            } catch(let err) {
+                print("err.localizedDescription", err.localizedDescription)
+            }
+            // 실패
+        case .failure(let err):
+            print(err.localizedDescription, "실패")
+        }
+        completion()
+    }
+}
+
+//MARK: - GuideVC
+
+struct InfoApplyBook: Codable {
+    let header: Header?
+    let data: infoApplyData?
+}
+
+struct infoApplyData: Codable {
+    let writer: String
+    let pubDate: Date
+    let publisher: Date
+}
+
+func getInfoApplyBook(bookPk: Int, completion: @escaping ()->()) {
+    let urlString = ""
+    
+    // HTTP Request
+    AF.request(urlString).responseJSON { (response) in
+        switch response.result {
+        case .success(let res):
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: res, options: .prettyPrinted)
+                let json = try JSONDecoder().decode(InfoApplyBook.self, from: jsonData)
+
+                DataManager.shared.infoApplyBook = json
+                print("succes!", DataManager.shared.infoApplyBook)
+            } catch(let err) {
+                print("err.localizedDescription", err.localizedDescription)
+            }
+            // 실패
+        case .failure(let err):
+            print(err.localizedDescription, "실패")
+        }
+        completion()
+    }
+}
+
+//MARK: - CollectUserInfoVC
+
+struct IsApplicableUser: Codable {
+    let header: Header?
+    let data: IsApplicableUserData?
+}
+
+struct IsApplicableUserData: Codable {
+    let canApply: Bool
+}
+
+func getIsApplicableUser(phoneNum: String, completion: @escaping ()->()) {
+    let urlString = ""
+    
+    // HTTP Request
+    AF.request(urlString).responseJSON { (response) in
+        switch response.result {
+        case .success(let res):
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: res, options: .prettyPrinted)
+                let json = try JSONDecoder().decode(IsApplicableUser.self, from: jsonData)
+
+                DataManager.shared.isApplicableUser = json
+                print("succes!", DataManager.shared.isApplicableUser)
+            } catch(let err) {
+                print("err.localizedDescription", err.localizedDescription)
+            }
+            // 실패
+        case .failure(let err):
+            print(err.localizedDescription, "실패")
+        }
+        completion()
+    }
+}
+
+
